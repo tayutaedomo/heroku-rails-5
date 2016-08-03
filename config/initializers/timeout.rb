956e8bd60c8889d456516db3e51b7ec6be073f2d
tayutaedomo <tayutaedomo@gmail.com>
2016-08-04 00:57:56 +0900

    Fix rack timeout

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,2 +1,4 @@
-Rack::Timeout.timeout = 20  # seconds
+# See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#timeout
+# Also: https://rubygems.org/gems/rack-timeout
+#Rack::Timeout.timeout = 20  # seconds
 
